0afa7207f01987eb17cad3bee07c62bccb1fd7ec
GeorgeAM4 <www.apazajorgem1@gmail.com>
2026-05-23 21:49:01 -0400

    Add files via upload

---
+++ proyecto_final_logica/proyecto_final.py
@@ -0,0 +1,17 @@
+print( "==================================")
+print("=SISTEMA DE GESTION DE ESTUDIANTES=")
+print("===================================")
+print("ELIGE TU ROL:")
+print("1. PROFESOR")
+print("2. ESTUDIANTE")
+opcion = ""
+while opcion !="1" and opcion != "2":
+    opcion = input("ELIGE TU ROL(1 o 2):").strip()
+if  opcion == "1":
+    print(" ELEGISTE PROFESOR, AQUI SE GESTIONA A LOS ESTUDIANTES.")
+    #AQUI FALTA COMPLETAR LAS OPCIONES PARA EL PROFESOR 
+elif  opcion == "2":
+    print("ELEGISTE ESTUDIANTE, AQUI PODRAS VER TUS NOTAS, TAREAS Y DEMAS")
+    #AQUI FALTA COMPLETAR LAS OPCIONES DEL ALUMNO 
+else:
+    print("OPCION NO VALIDA, REINICI LA PAGINA.")
